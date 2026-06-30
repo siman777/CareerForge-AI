94280a96663516e8dc3522a120b670b1f9dcc5e0
Siman D Ranjan Raj <127052933+siman777@users.noreply.github.com>
2026-06-30 16:13:37 +0530

    Fix condition check for updateResult in useEffect

--- app/(main)/onboarding/_components/onboarding-form.jsx
+++ app/(main)/onboarding/_components/onboarding-form.jsx
@@ -66,7 +66,7 @@ const OnboardingForm = ({ industries }) => {
   };
 
   useEffect(() => {
-    if (updateResult?.success && !updateLoading) {
+    if (updateResult && !updateLoading) {
       toast.success("Profile completed successfully!");
       router.push("/dashboard");
       router.refresh();
